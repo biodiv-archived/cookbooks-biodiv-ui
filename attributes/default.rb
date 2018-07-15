f70990542a547ca5e20a9b3a6814c0b9e27aa0a4
Sravanthi <sravanthi@strandls.com>
2018-07-15 21:48:45 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,7 @@ default[:biodivUi][:directory] = "/usr/local/src"
 
 default[:biodivUi][:link]      = "https://codeload.github.com/strandls/#{biodivUi.repository}/zip/#{biodivUi.version}"
 default[:biodivUi][:extracted] = "#{biodivUi.directory}/#{biodivUi.appname}-#{biodivUi.version}"
-#default[:biodivUi][:war]       = "#{biodivUi.extracted}/build/libs/#{biodivUi.appname}.war"
+default[:biodivUi][:build]       = "#{biodivUi.extracted}/build"
 default[:biodivUi][:download]  = "#{biodivUi.directory}/#{biodivUi.repository}-#{biodivUi.version}.zip"
 
 default[:biodivUi][:home] = "/usr/local/biodiv-ui"
-
